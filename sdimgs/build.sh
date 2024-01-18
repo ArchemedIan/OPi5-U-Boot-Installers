0bc66535e87875aa5f5912c1f970a3259b3d0938
ArchemedIan <105951210+ArchemedIan@users.noreply.github.com>
2024-01-18 10:05:11 -0500

    Update build.sh

--- sdimgs/build.sh
+++ sdimgs/build.sh
@@ -49,7 +49,7 @@ mv $bootorderSpiDir/out/u-boot-spi-inst-$ubootRef-${boardName}__${order}.bin .
 fallocate -l 16M sdimg-u-boot-spi-inst-${ubootRef}-${boardName}__${order}.img
 
 losetup -f sdimg-u-boot-spi-inst-${ubootRef}-${boardName}__${order}.img
-NewImgloopdev=`losetup |grep __ | awk '{print $1}'`
+NewImgloopdev=`losetup |grep sdimg-u-boot-spi-inst-${ubootRef}-${boardName}__${order} | awk '{print $1}'`
 echo NewImgloopdev is $NewImgloopdev
 dd if=/dev/zero of=${NewImgloopdev} count=4096 bs=512
 parted --script ${NewImgloopdev} -- \
@@ -60,9 +60,16 @@ partprobe $NewImgloopdev
 mkfs.ext4 -L SpiInst ${NewImgloopdev}p1
 partprobe $NewImgloopdev
 tune2fs -O ^metadata_csum ${NewImgloopdev}p1
-
-
 dd if=u-boot-spi-inst-$ubootRef-${boardName}__${order}.bin of=sdimg-u-boot-spi-inst-${ubootRef}-${boardName}__${order}.img seek=1 bs=32k conv=fsync
+sync
+mkdir 1
+mount ${NewImgloopdev}p1 1
+cp $sdimgOutDir/u-boot-${ubootRef}-${boardName}__${order}.bin 1
+umount ${NewImgloopdev}p1
+partx -d ${NewImgloopdev}p1
+losetup -d ${NewImgloopdev}
+sync
+sleep 5
 mv sdimg-u-boot-spi-inst-${ubootRef}-${boardName}__${order}.img $sdimgOutDir/
 mv u-boot-spi-inst-$ubootRef-${boardName}__${order}.bin $sdimgOutDir/
 #rm $bootorderDir/out/u-boot-spi-inst-${ubootRef}-${boardName}__${order}.bin
