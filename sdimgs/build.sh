acd541cc3bad922266789881097bbd89a9eb965f
ArchemedIan <105951210+ArchemedIan@users.noreply.github.com>
2024-01-18 06:50:10 -0500

    Update build.sh

--- sdimgs/build.sh
+++ sdimgs/build.sh
@@ -2,7 +2,7 @@
 
 rootdir="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 bootorderDir=$rootdir/../bootorder
-bootorderSpiDir=$rootdir/../bootorder-spi-inst
+bootorderSpiDir=$rootdir/../spi-inst
 sdimgOutDir=$rootdir/sdmimgout
 sdimgSpiOutDir=$rootdir/sdmimgspiout
 
@@ -34,7 +34,20 @@ rm $bootorderDir/out/u-boot-${ubootRef}-${boardName}__${order}.bin
 cd $rootdir
 
 #build bootordered sdimg SPI installer
+cd $bootorderSpiDir
+chmod a+x ./build.sh 
+./build.sh "${ubootRef}" "${ubootRepo}" "${boardconfig}" "${order}" "${ubootCustRef}" "${ubootCustRepo}" "${custOrder}" "${boardName}"
+cd $rootdir
 
 #make bootordered sdimg SPI installer
-#cp $bootorderDir/out/u-boot-${ubootRef}-${boardName}-spi__${order}.bin .
+mkdir sdimgspi
+cd sdimgspi
+mv $bootorderSpiDir/out/u-boot-spi-inst-$ubootRef-${boardName}__$orderUnder.bin .
+fallocate -l 16M sdimg-u-boot-${ubootRef}-${boardName}__${order}.img
+dd if=u-boot-${ubootRef}-${boardName}__${order}.bin of=sdimg-u-boot-${ubootRef}-${boardName}__${order}.img seek=1 bs=32k conv=fsync
+mv sdimg-u-boot-${ubootRef}-${boardName}__${order}.img $sdimgOutDir/
+rm $bootorderDir/out/u-boot-${ubootRef}-${boardName}__${order}.bin
+cd $rootdir
+
 #upload 
+ls $sdimgOutDir/
